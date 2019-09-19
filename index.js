3492717caf1dfe9dea69b274a9efa6dcca9b72c7
Nstammewar <nstammewar@gmail.com>
2019-09-19 20:01:02 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import App1 from './App1';
 import './style.css';
 
 
-render(<App />, document.getElementById('root'));
+render(<App1 />, document.getElementById('root'));
